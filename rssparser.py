ece148aae3896366072dbd28248d21c7c29c669c
CapAgenda <capitalistagenda@pm.me>
2022-08-26 13:37:27 -0700

    parser pagination and workflow manual fix

--- rssparser.py
+++ rssparser.py
@@ -1,13 +1,16 @@
 import feedparser
 
-url = 'https://comicstripblog.com/feed/'
+allurls = 'https://comicstripblog.com/feed/?page='
 
-f = feedparser.parse(url)
+i=1
+while (i<=5):
+    urlpage = (allurls + str(i))
+    f = feedparser.parse(urlpage)
+    for feedentry in f.entries:
+        print(feedentry.title)
+        print(feedentry.media_content[1].get('url'))
+    i=i+1
+else:
+    print("End of the loop")
+ 
 
-""" Print number of entries from feed """
-print(len(f['entries']))
-
-""" Get specific data for multiple entries """
-for feedentry in f.entries:
-    print(feedentry.title)
-    print(feedentry.media_content[1].get('url'))
