a14ca54be92123a073be6c057b957495ee71f78c
CapAgenda <capitalistagenda@pm.me>
2022-08-27 17:27:14 -0700

    test1 of workflow to upload images

--- rssparser.py
+++ rssparser.py
@@ -2,15 +2,23 @@ import feedparser
 
 allurls = 'https://comicstripblog.com/feed/?paged='
 
+#Create empty list
+comiclist = []
+
+# Loop through pages of feeds and add dictionary of title:url for each comic
 i=1
 while (i<=5):
     urlpage = (allurls + str(i))
     f = feedparser.parse(urlpage)
     for feedentry in f.entries:
-        print(feedentry.title)
-        print(feedentry.media_content[1].get('url'))
+        comic = {feedentry.title : feedentry.media_content[1].get('url')}
+        comiclist.append(comic)
+      
     i=i+1
 else:
     print("End of the loop")
+
+#print list
+print (comiclist)
  
 
